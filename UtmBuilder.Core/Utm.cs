41eac67f07c6cc8781fd6562f451b3b4bc59583a
black-adm <mattheuscontato17@gmail.com>
2024-08-02 11:53:20 -0300

    Adicionar arquivos de projeto.

---
+++ UtmBuilder.Core/Utm.cs
@@ -0,0 +1,12 @@
+﻿namespace UtmBuilder.Core
+{
+  public class Utm
+  {
+    public string Id { get; set; } = string.Empty;
+    public string Url { get; set; } = string.Empty;
+    public string Source { get; set; } = string.Empty;
+    public string Medium { get; set; } = string.Empty;
+    public string Term { get; set; } = string.Empty;
+    public string Content { get; set; } = string.Empty;
+  }
+}
